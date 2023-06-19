2e8e732f0ccc98e4bf5cb80167ea8327145656e6
Siroshun09 <siro.shun8@gmail.com>
2023-06-20 00:28:45 +0900

    chore: make PlatformHelper final class

--- src/main/java/net/okocraft/scoreboard/util/PlatformHelper.java
+++ src/main/java/net/okocraft/scoreboard/util/PlatformHelper.java
@@ -19,7 +19,7 @@ import java.util.concurrent.Executor;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-public class PlatformHelper {
+public final class PlatformHelper {
 
     private static final boolean FOLIA;
     private static final boolean ASYNC_SCHEDULER;
